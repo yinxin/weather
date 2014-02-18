4bf32346ea802c659a7beed035c61d9cd8e95fe3
Mike Yin <mykeyin@gmail.com>
2014-02-18 17:13:58 +0800

    Update and rename test.js to GBKencode.js

--- GBKencode.js
+++ GBKencode.js
@@ -4,10 +4,10 @@ iframe.setAttribute("style","display:none;visibility:hidden;");
 document.body.appendChild(iframe);  
 var d=iframe.contentWindow.document;  
 d.charset=d.characterSet="GBK";  
-function getGBKEscape(s) {
+function getGBKEscape(s) {  
     d.write("<body><a href='?"+s+"'>X</a></body>");  
     d.close();  
     var url=d.body.firstChild.href;  
     return url.substr(url.lastIndexOf("?")+1);  
-};
-alert(getGBKEscape('烟火花'))
+}
+alert(getGBKEscape('烟火花'));
