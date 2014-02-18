b8f06c40c19dab3909dd2a670c36c6cc714f1e89
Mike Yin <mykeyin@gmail.com>
2014-02-18 17:08:33 +0800

    Create test.js

---
+++ test.js
@@ -0,0 +1,13 @@
+var iframe=document.createElement("iframe");  
+iframe.src="about:blank";  
+iframe.setAttribute("style","display:none;visibility:hidden;");  
+document.body.appendChild(iframe);  
+var d=iframe.contentWindow.document;  
+d.charset=d.characterSet="GBK";  
+function getGBKEscape(s) {  
+    d.write("<body><a href='?"+s+"'>X</a></body>");  
+    d.close();  
+    var url=d.body.firstChild.href;  
+    return url.substr(url.lastIndexOf("?")+1);  
+};
+alert("getGBKEscape('烟火花')")
